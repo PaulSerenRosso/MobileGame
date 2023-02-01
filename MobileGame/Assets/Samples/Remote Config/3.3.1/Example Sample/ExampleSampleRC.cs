ffc9b3f3bad5a44b3c9cd438e3b2a1edc891c6b6
Paul SEREN-ROSSO <p.serenrosso@rubika-edu.com>
2023-02-01 18:27:46 +0100

    Int remote Config

---
+++ MobileGame/Assets/Samples/Remote Config/3.3.1/Example Sample/ExampleSampleRC.cs
@@ -0,0 +1,86 @@
+// -----------------------------------------------------------------------------
+//
+// This sample example C# file can be used to quickly utilise usage of Remote Config APIs
+// For more comprehensive code integration, visit https://docs.unity3d.com/Packages/com.unity.remote-config@latest
+//
+// -----------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Unity.Services.RemoteConfig;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+
+
+public class ExampleSampleRC : MonoBehaviour
+{
+
+    public struct userAttributes {}
+    public struct appAttributes {}
+
+    async Task InitializeRemoteConfigAsync()
+    {
+            // initialize handlers for unity game services
+            await UnityServices.InitializeAsync();
+
+            // options can be passed in the initializer, e.g if you want to set analytics-user-id use two lines from below:
+            // var options = new InitializationOptions().SetOption("com.unity.services.core.analytics-user-id", "my-user-id-123");
+            // await UnityServices.InitializeAsync(options);
+
+            // remote config requires authentication for managing environment information
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+    }
+
+    async Task Start()
+    {
+        // initialize Unity's authentication and core services, however check for internet connection
+        // in order to fail gracefully without throwing exception if connection does not exist
+        if (Utilities.CheckForInternetConnection())
+        {
+            await InitializeRemoteConfigAsync();
+        }
+
+        RemoteConfigService.Instance.FetchCompleted += ApplyRemoteSettings;
+        RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
+
+        // -- Example on how to fetch configuration settings using filter attributes:
+        // var fAttributes = new filterAttributes();
+        // fAttributes.key = new string[] { "sword","cannon" };
+        // RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes(), fAttributes);
+
+        // -- Example on how to fetch configuration settings if you have dedicated configType:
+        // var configType = "specialConfigType";
+        // RemoteConfigService.Instance.FetchConfigs(configType, new userAttributes(), new appAttributes());
+        // -- Configuration can be fetched with both configType and fAttributes passed
+        // RemoteConfigService.Instance.FetchConfigs(configType, new userAttributes(), new appAttributes(), fAttributes);
+
+        // -- All examples from above will also work asynchronously, returning Task<RuntimeConfig>
+        // await RemoteConfigService.Instance.FetchConfigsAsync(new userAttributes(), new appAttributes());
+        // await RemoteConfigService.Instance.FetchConfigsAsync(new userAttributes(), new appAttributes(), fAttributes);
+        // await RemoteConfigService.Instance.FetchConfigsAsync(configType, new userAttributes(), new appAttributes());
+        // await RemoteConfigService.Instance.FetchConfigsAsync(configType, new userAttributes(), new appAttributes(), fAttributes);
+    }
+
+    void ApplyRemoteSettings(ConfigResponse configResponse)
+    {
+
+        switch (configResponse.requestOrigin)
+        {
+            case ConfigOrigin.Default:
+                Debug.Log("Default values will be returned");
+                break;
+            case ConfigOrigin.Cached:
+                Debug.Log("Cached values loaded");
+                break;
+            case ConfigOrigin.Remote:
+                Debug.Log("Remote Values changed");
+                Debug.Log("RemoteConfigService.Instance.appConfig fetched: " + RemoteConfigService.Instance.appConfig.config.ToString());
+                break;
+        }
+
+    }
+
+}
