352d3ea16cafebf538dba7d449db66f42e58ef84
Paul SEREN-ROSSO <p.serenrosso@rubika-edu.com>
2023-02-21 17:29:11 +0100

    Fix : Physics Movement

--- MobileGame/Assets/Scripts/Core/Actions/MovementAction.cs
+++ MobileGame/Assets/Scripts/Core/Actions/MovementAction.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Action
 {
-    public class MovementAction : MonoBehaviour, IAction, IUpdatable
+    public class MovementAction : MonoBehaviour, IAction, IFixedUpdate
     {
         public Vector3 Destination;
         [SerializeField] private MovementSO _movementSO;
@@ -12,29 +12,30 @@ namespace Action
         private float _timer;
         private bool _isMoving;
         private float _ratioTime;
+        [SerializeField] private Rigidbody rb;
 
-        public void OnUpdate()
+        public void OnFixedUpdate()
         {
             if (_timer >= _movementSO.MaxTime)
             {
                 _isMoving = false;
                 transform.position = Destination;
-                UpdateManager.UnRegister(this);
+                FixedUpdateManager.UnRegister(this);
                 return;
             }
-            _timer += Time.deltaTime;
+            _timer += Time.fixedDeltaTime;
             _ratioTime = _timer / _movementSO.MaxTime;
-            transform.position = Vector3.Lerp(_startPosition, Destination, _movementSO.CurvePosition.Evaluate(_ratioTime));
+            rb.position = Vector3.Lerp(_startPosition, Destination, _movementSO.CurvePosition.Evaluate(_ratioTime));
         }
 
         public bool IsInAction { get => _isMoving; }
 
         public void MakeAction()
         {
-            _startPosition = transform.position;
+            _startPosition = rb.position;
             _isMoving = true;
             _timer = 0;
-            UpdateManager.Register(this);
+            FixedUpdateManager.Register(this);
             MakeActionEvent?.Invoke();
         }
 
