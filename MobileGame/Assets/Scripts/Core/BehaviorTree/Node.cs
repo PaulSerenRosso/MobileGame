654714a809567bfe0d6ad3bbeef05401577e1ace
Maxime Duflot <maximedft@outlook.com>
2023-02-22 21:22:20 +0100

    feat: add generic behaviortree;

---
+++ MobileGame/Assets/Scripts/Core/BehaviorTree/Node.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+
+namespace BehaviorTree
+{
+    public enum NodeState
+    {
+        RUNNING,
+        SUCCESS,
+        FAILURE
+    }
+
+    public class Node
+    {
+        public Node Parent;
+
+        protected NodeState _state;
+        protected List<Node> _children = new();
+
+        private Dictionary<string, object> _dataContext = new();
+
+        public Node()
+        {
+            Parent = null;
+        }
+
+        public Node(List<Node> children)
+        {
+            foreach (Node child in children)
+                Attach(child);
+        }
+
+        private void Attach(Node node)
+        {
+            node.Parent = this;
+            _children.Add(node);
+        }
+
+        public virtual NodeState Evaluate() => NodeState.FAILURE;
+
+        public void SetData(string key, object value)
+        {
+            _dataContext[key] = value;
+        }
+
+        public object GetData(string key)
+        {
+            if (_dataContext.TryGetValue(key, out var value))
+                return value;
+
+            Node node = Parent;
+            while (node != null)
+            {
+                value = node.GetData(key);
+                if (value != null)
+                    return value;
+                node = node.Parent;
+            }
+
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+            if (_dataContext.ContainsKey(key))
+            {
+                _dataContext.Remove(key);
+                return true;
+            }
+
+            Node node = Parent;
+            while (node != null)
+            {
+                bool cleared = node.ClearData(key);
+                if (cleared)
+                    return true;
+                node = node.Parent;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ MobileGame/Assets/Scripts/Core/BehaviorTree/Tree.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BehaviorTree
+{
+    public abstract class Tree : MonoBehaviour
+    {
+        private Node _root;
+
+        protected void Start()
+        {
+            _root = SetupTree();
+        }
+
+        private void Update()
+        {
+            _root?.Evaluate();
+        }
+
+        protected abstract Node SetupTree();
+    }
+}
