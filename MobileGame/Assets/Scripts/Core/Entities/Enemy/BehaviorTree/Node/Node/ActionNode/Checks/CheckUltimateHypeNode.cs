93035bfa96ef6dcbb19ecdcd1eef5b32c1f6f2df
Maxime Duflot <maximedft@outlook.com>
2023-04-12 21:46:51 +0200

    feat: add ultimate enemy;

---
+++ MobileGame/Assets/Scripts/Core/Entities/Enemy/BehaviorTree/Node/Node/ActionNode/Checks/CheckUltimateHypeNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BehaviorTree.SO.Actions;
+using Service.Hype;
+
+namespace BehaviorTree.Nodes.Actions
+{
+    public class CheckUltimateHypeNode : ActionNode
+    {
+        private CheckUltimateHypeNodeSO _so;
+        private CheckUltimateHypeNodeDataSO _data;
+        private EnemyManager _enemyManager;
+        private IHypeService _hypeService;
+
+        public override NodeSO GetNodeSO()
+        {
+            return _so;
+        }
+
+        public override void SetNodeSO(NodeSO nodeSO)
+        {
+            _so = (CheckUltimateHypeNodeSO)nodeSO;
+            _data = (CheckUltimateHypeNodeDataSO)_so.Data;
+        }
+
+        public override void Evaluate()
+        {
+            base.Evaluate();
+            if (_hypeService.GetUltimateAreaEnemy()) _enemyManager.CanUltimateEvent?.Invoke();
+            State = BehaviorTreeEnums.NodeState.SUCCESS;
+            ReturnedEvent?.Invoke();
+        }
+
+        public override void SetDependencyValues(
+            Dictionary<BehaviorTreeEnums.TreeExternValues, object> externDependencyValues,
+            Dictionary<BehaviorTreeEnums.TreeEnemyValues, object> enemyDependencyValues)
+        {
+            _enemyManager = (EnemyManager)enemyDependencyValues[BehaviorTreeEnums.TreeEnemyValues.EnemyManager];
+            _hypeService = (IHypeService)externDependencyValues[BehaviorTreeEnums.TreeExternValues.HypeService];
+        }
+
+        public override ActionNodeDataSO GetDataSO()
+        {
+            return _data;
+        }
+    }
+}
---
+++ MobileGame/Assets/Scripts/Core/Entities/Enemy/BehaviorTree/SO/ActionsSO/ActionsDataSO/Checks/CheckUltimateHypeNodeDataSO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BehaviorTree.Nodes.Actions;
+using UnityEngine;
+
+namespace BehaviorTree.SO.Actions
+{
+    [CreateAssetMenu(menuName = "BehaviorTree/Data/Checks/UltimateHypeNodeDataSO",
+        fileName = "new CH_UltimateHype_Spe_Data")]
+    public class CheckUltimateHypeNodeDataSO: ActionNodeDataSO
+    {
+        protected override void SetDependencyValues()
+        {
+            EnemyValues = new[]
+            {
+                BehaviorTreeEnums.TreeEnemyValues.EnemyManager
+            };
+            ExternValues = new[]
+            {
+                BehaviorTreeEnums.TreeExternValues.HypeService
+            };
+        }
+
+        public override Type GetTypeNode()
+        {
+            return typeof(CheckUltimateHypeNode);
+        }
+    }
+}
