495b7e32f6f57fa9531292a7b9e50f4500932197
Paul Seren-Rosso <p.serenrosso@rubika-edu.com>
2023-04-26 16:11:19 +0200

    feat : create decorator delay returner Node

---
+++ MobileGame/Assets/Scripts/Core/Entities/Enemy/BehaviorTree/Node/Node/InnerNode/Decorator/DecoratorDelayReturnerNode.cs
@@ -0,0 +1,37 @@
+﻿using BehaviorTree.SO.Decorator;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace BehaviorTree.Nodes.Decorator
+{
+    [CreateAssetMenu(menuName = "BehaviorTree/Decorator/DelayReturnerSO",
+        fileName = "new Tree_D_DelayReturner_Spe")]
+    public class DecoratorDelayReturnerNode : DecoratorNode 
+    {
+        private DecoratorDelayReturnerNodeSO _so;
+
+        public override NodeSO GetNodeSO()
+        {
+            return _so;
+        }
+
+        public override void SetNodeSO(NodeSO nodeSO)
+        {
+            _so = (DecoratorDelayReturnerNodeSO)nodeSO;
+        }
+
+        public override async void Evaluate()
+        {
+            State = _so.ReturnState;
+            ReturnedEvent?.Invoke();
+            await UniTask.Delay(_so.TimeBeforeToReadChild);
+            Child.Evaluate();
+        }
+
+        public override void EvaluateChild()
+        {
+          
+        }
+    }
+    
+}
