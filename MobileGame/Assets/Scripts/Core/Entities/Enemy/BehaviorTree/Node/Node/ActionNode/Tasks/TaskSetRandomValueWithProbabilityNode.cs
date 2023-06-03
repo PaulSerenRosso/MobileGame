e6801156444b9e5f784941b14cfb054f3fce9042
Maxime <32262840+duflosky@users.noreply.github.com>
2023-06-03 10:23:49 +0200

    fix: remove debugs;

--- MobileGame/Assets/Scripts/Core/Entities/Enemy/BehaviorTree/Node/Node/ActionNode/Tasks/TaskSetRandomValueWithProbabilityNode.cs
+++ MobileGame/Assets/Scripts/Core/Entities/Enemy/BehaviorTree/Node/Node/ActionNode/Tasks/TaskSetRandomValueWithProbabilityNode.cs
@@ -35,7 +35,6 @@ namespace BehaviorTree.Nodes.Actions
             for (var index = 0; index < _currentProbabilities.Count; index++)
             {
                 var currentProbability = _currentProbabilities[index];
-                Debug.Log($"{_so.InternValues[index].Key} : {currentProbability}");
             }
 
             if (_randomProbilityIndexes.Count == 0)
@@ -47,7 +46,6 @@ namespace BehaviorTree.Nodes.Actions
                 var randomPick = RandomHelper.PickRandomElementIndex(_currentProbabilities.ToArray());
                 CollectionHelper.AddOrSet(ref Sharer.InternValues, _so.InternValues[^2].HashCode,
                     _randomProbilityIndexes[randomPick]);
-                Debug.Log($"Pick: {_so.InternValues[randomPick].Key}");
                 _randomProbilityIndexes.RemoveAt(randomPick);
                 _currentProbabilities.RemoveAt(randomPick);
                 State = BehaviorTreeEnums.NodeState.SUCCESS;
